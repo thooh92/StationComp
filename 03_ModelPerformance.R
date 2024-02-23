86e453fe2f87a5776dc3afad4d5219a54b9c5b16
Thomas Ohnemus <thomas.ohnemus@ufz.de>
2024-02-23 09:59:04 +0100

    Initialized 03_ModelPerformance.R

---
+++ 03_ModelPerformance.R
@@ -0,0 +1,10 @@
+## Author: Thomas Ohnemus
+## Date: 23/02/2024
+## Evaluation of Model Performance compared to Station Data
+
+# prepare script
+rm(list = ls())
+
+
+library(data.table)
+library(tidyverse)
